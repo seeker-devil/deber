f28ff0131c24a694cd3d3e9eb69431598e9fc392
DANIEL HIDALGO <da.hidalgoc@uea.edu.ec>
2025-02-23 22:29:58 -0500

    Mensaje de commit

---
+++ DEBER 10/producto.py
@@ -0,0 +1,17 @@
+# producto.py
+class Producto:
+    def __init__(self, id, nombre):
+        self.id = id
+        self.nombre = nombre
+
+    def get_id(self):
+        return self.id
+
+    def set_id(self, id):
+        self.id = id
+
+    def get_nombre(self):
+        return self.nombre
+
+    def set_nombre(self, nombre):
+        self.nombre = nombre
